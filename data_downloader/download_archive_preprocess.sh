eb3b03488776cdc5b5427ea3776f480520796593
hamed <hamedlakers@gmail.com>
2019-09-05 22:11:23 +0430

    modify bash script

--- data_downloader/download_archive_preprocess.sh
+++ data_downloader/download_archive_preprocess.sh
@@ -1,25 +1,25 @@
 # Requires 2GB of free disk space at most.
 SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
 DL_PATH="$SCRIPTPATH"/download/
-#mkdir -p "$DL_PATH"
+mkdir -p "$DL_PATH"
 echo "Downloading files to "$DL_PATH""
 # See: https://blog.archive.org/2012/04/26/downloading-in-bulk-using-wget/
-#wget -r -H -nc -nH --cut-dir=1 -A .ogg -R *_vbr.mp3 -e robots=off -P "$DL_PATH" -l1 -i ./itemlist.txt -B 'http://archive.org/download/'
-#echo "Organizing files and folders"
-#mv "$DL_PATH"*/*.ogg "$DL_PATH"
-#rm -rf "$DL_PATH"*/
+wget -r -H -nc -nH --cut-dir=1 -A .ogg -R *_vbr.mp3 -e robots=off -P "$DL_PATH" -l1 -i ./itemlist.txt -B 'http://archive.org/download/'
+echo "Organizing files and folders"
+mv "$DL_PATH"*/*.ogg "$DL_PATH"
+rm -rf "$DL_PATH"*/
 #echo "Converting from OGG to 16Khz, 16bit mono-channel WAV"
 # Next line with & executes in a forked shell in the background. That's parallel and not recommended.
 
-#for file in "$DL_PATH"*.ogg; do
-	#ffmpeg -i "$file" -ar 16000 -ac 1 "$DL_PATH""`basename "$file" .ogg`.wav"
-#done 
-#echo "Cleaning up"
-#rm "$DL_PATH"*.ogg
+for file in "$DL_PATH"*.ogg; do
+	ffmpeg -i "$file" -ar 16000 -ac 1 "$DL_PATH""`basename "$file" .ogg`.wav"
+done 
+echo "Cleaning up"
+rm "$DL_PATH"*.ogg
 
-#echo "Preprocessing"
-#python preprocess.py "$DL_PATH"
-#echo "Done!"
+echo "Preprocessing"
+python preprocess.py "$DL_PATH"
+echo "Done!"
 
 echo "Writing datasets"
 python _2npy.py
